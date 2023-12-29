88c2713febbcdda73361a15f30fd0fff7b6439b3
sr1cky <sw3etky@hotmail.com>
2023-12-29 02:25:26 -0500

    Add file: IPTV-tvbox.txt

--- autorun.sh
+++ autorun.sh
@@ -3,6 +3,9 @@
 # 获取脚本所在的目录
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+# 进入到脚本所在的目录
+cd "$SCRIPT_DIR"
+
 # 1. 进入 auto-iptv 目录并执行 run.sh
 echo "执行 auto-iptv/run.sh..."
 (cd "$SCRIPT_DIR/../auto-iptv" && ./run.sh)
@@ -10,9 +13,26 @@ echo "执行 auto-iptv/run.sh..."
 # 检查上一步是否成功，成功后执行下一步
 if [ $? -eq 0 ]; then
     # 2. 执行当前目录下的 push.sh
-    echo "执行当前目录下的 push.sh..."
-(cd "../TVBOX" && ./push.sh)
-    echo "脚本执行完成。"
+
+    # 设置 GitHub 用户名和仓库信息
+    USERNAME="sr1cky"
+    REPO_NAME="TVBOX"
+
+    # 设置本地文件路径和目标分支
+    LOCAL_FILE="IPTV-tvbox.txt"
+    BRANCH_NAME="main"
+
+    # 添加文件到本地仓库
+    git add .
+
+    # 提交更改
+    git commit -m "Add file: $LOCAL_FILE"
+
+    # 推送到 GitHub
+    git push origin $BRANCH_NAME
+
+    echo "文件推送完成。"
+
 else
     echo "执行 auto-iptv/run.sh 失败，无法执行 push.sh。"
 fi
